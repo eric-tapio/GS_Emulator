c5a4c32430afa320c196fb0679ff454a312d74c2
eric-tapio <etapio@alumni.stanford.edu>
2026-01-19 21:07:03 -0800

    Update pip install to include pyserial

--- pip_install_dependencies.sh
+++ pip_install_dependencies.sh
@@ -1,6 +1,10 @@
  #!/bin/bash
 
 python3 -m pip install --upgrade pip
+or 
+python.exe -m pip install --upgrade pip
+
 pip install uvicorn
 pip install python-socketio
 pip install aiohttp
+pip install pyserial
